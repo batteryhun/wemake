197124db69e68913913fdd713995d9d6467fadbc
batteryhun <100200467+batteryhun@users.noreply.github.com>
2025-03-17 13:35:23 +0900

    Update weekly-leaderboard-page.tsx

--- app/features/products/pages/weekly-leaderboard-page.tsx
+++ app/features/products/pages/weekly-leaderboard-page.tsx
@@ -1,32 +1,121 @@
-import type { Route } from "~/types/routes";
-import type { MetaFunction } from "@remix-run/react";
-import { useParams } from "react-router";
+import { DateTime } from "luxon";
+import type { Route } from "./+types/weekly-leaderboard-page";
+import { data, isRouteErrorResponse, Link } from "react-router";
+import { date, z } from "zod";
+import { Hero } from "~/common/components/hero";
+import { ProductCard } from "../components/product-card";
+import { Button } from "~/common/components/ui/button";
+import { ProductPagination } from "~/common/components/product-pagination";
 
-export function loader({ request, params }: Route.LoaderArgs) {
-  return {
-    leaderboard: [],
-  };
-}
+const paramsSchema = z.object({
+  year: z.coerce.number(),
+  week: z.coerce.number(),
+});
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "Weekly Leaderboard | Your App Name" },
-    { name: "description", content: "Weekly product leaderboard" },
-  ];
+export const loader = ({ params }: Route.LoaderArgs) => {
+  const { success, data: parsedData } = paramsSchema.safeParse(params);
+  if (!success) {
+    throw data(
+      {
+        error_code: "invalid_params",
+        message: "Invalid params",
+      },
+      { status: 400 }
+    );
+  }
+  const date = DateTime.fromObject({
+    weekYear: parsedData.year,
+    weekNumber: parsedData.week,
+  }).setZone("Asia/seoul");
+  if (!date.isValid) {
+    throw data(
+      {
+        error_code: "invalid_date",
+        message: "Invalid date",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+  if (date > DateTime.now().startOf("day")) {
+    throw data(
+      {
+        error_code: "future_date",
+        message: "Future date",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+  return { ...parsedData };
 };
 
 export default function WeeklyLeaderboardPage({
   loaderData,
 }: Route.ComponentProps) {
-  const { year, week } = useParams();
-  const { leaderboard } = loaderData;
-
+  const urlDate = DateTime.fromObject({
+    weekYear: loaderData.year,
+    weekNumber: loaderData.week,
+  });
+  const previousWeek = urlDate.minus({ week: 1 });
+  const nextWeek = urlDate.plus({ week: 1 });
+  const isToday = urlDate.equals(DateTime.now().startOf("week"));
   return (
-    <div className="container py-8">
-      <h1 className="text-3xl font-bold mb-6">
-        Week {week}, {year}
-      </h1>
-      {/* Weekly leaderboard content */}
+    <div>
+      <Hero
+        title={`Best of week ${urlDate
+          .startOf("week")
+          .toLocaleString(DateTime.DATE_SHORT)} - ${urlDate
+          .endOf("week")
+          .toLocaleString(DateTime.DATE_SHORT)}`}
+      />
+      <div className="flex gap-4 justify-between">
+        <Button variant="link" asChild>
+          <Link
+            to={`/products/leaderboards/weekly/${previousWeek.weekYear}/${previousWeek.weekNumber}`}
+          >
+            &larr; {previousWeek.toLocaleString(DateTime.DATE_MED)}
+          </Link>
+        </Button>
+        {isToday ? null : (
+          <Button variant="link" asChild>
+            <Link
+              to={`/products/leaderboards/weekly/${nextWeek.weekYear}/${nextWeek.weekNumber}`}
+            >
+              {nextWeek.toLocaleString(DateTime.DATE_MED)} &rarr;
+            </Link>
+          </Button>
+        )}
+      </div>
+      <div className="flex flex-col gap-4 w-full max-w-screen-md mx-auto">
+        {Array.from({ length: 10 }).map((_, index) => (
+          <ProductCard
+            id="productId"
+            name="Product Name"
+            description="Product Description"
+            commentsCount={12}
+            viewsCount={1254}
+            upvotesCount={120}
+          />
+        ))}
+      </div>
+      <ProductPagination totalPages={10}></ProductPagination>
     </div>
   );
+}
+
+export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div>
+        {error.data.error_code} / {error.data.message}
+      </div>
+    );
+  }
+  if (error instanceof Error) {
+    return <div>{error.message}</div>;
+  }
+  return <div>Unknown error in leaderboard page</div>;
 }
